f18118a9017348dcd51b76aaf07ea6b91f630d36
Kaynecoirir <arseniy.grodno@mail.ru>
2022-12-23 20:25:23 +0300

    Initial commit

---
+++ Cell.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LestaAcademyWork1
+{
+	class Cell
+	{
+		public int Value;
+		public Cell Top;
+		public Cell Right;
+		public Cell Down;
+		public Cell Left;
+
+		public Cell(int v = 0)
+		{
+			Value = v;
+			Top = null;
+			Right = null;
+			Down = null;
+			Left = null;
+		}
+
+		public void Connect(int move, Cell c)
+		{
+			if (move == 1) { Top = c; Top.Down = this; }
+			if (move == 2) { Right = c; Right.Left = this; }
+			if (move == 3) { Down = c; Down.Top = this; }
+			if (move == 4) { Left = c; Left.Right = this; }
+		}
+
+		public bool AbleToMove(int move)
+		{
+			if (this.Value < 1) return false;
+			Cell curmove = null;
+
+			if(move == 0)
+			{
+				for(int i = 1; i < 5; i++)
+				{
+					if (AbleToMove(i)) return true;
+				}
+			}
+
+			if (move == 1) curmove = Top;
+			else if (move == 2) curmove = Right;
+			else if (move == 3) curmove = Down;
+			else if (move == 4) curmove = Left;
+
+			if (curmove == null) return false;
+			if (curmove.Value == 0) return true;
+			return false;
+		}
+
+		public Cell Move(int move)
+		{
+			if(AbleToMove(move))
+			{
+				Cell curmove = null;
+				if (move == 1) curmove = Top;
+				else if (move == 2) curmove = Right;
+				else if (move == 3) curmove = Down;
+				else if (move == 4) curmove = Left;
+				int k = curmove.Value;
+				curmove.Value = this.Value;
+				this.Value = k;
+
+				return curmove;
+			}
+			return null;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace LestaAcademyWork1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[][] array = new int[][] {
+				new int[]{1, -1, 2, -1, 3},
+				new int[]{1,  0, 2,  0, 3},
+				new int[]{1, -1, 2, -1, 3},
+				new int[]{1,  0, 2,  0, 3},
+				new int[]{1, -1, 2, -1, 3}};
+			Cell[][] CellMap = new Cell[5][]
+			{
+				new Cell[5],
+				new Cell[5],
+				new Cell[5],
+				new Cell[5],
+				new Cell[5]
+			};
+
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					CellMap[i][j] = new Cell(array[i][j]);
+					if (i > 0) CellMap[i][j].Connect(1, CellMap[i - 1][j]);
+					if (j > 0) CellMap[i][j].Connect(4, CellMap[i][j - 1]);
+				}
+			}
+
+			Random rnd = new Random();
+			Cell oldcell = null;
+			for (int n = 0; n < 1000; n++)
+			{
+				int ni = rnd.Next(0, 5);
+				int nj = rnd.Next(0, 5);
+				if (CellMap[ni][nj] == oldcell || !CellMap[ni][nj].AbleToMove(0))
+				{
+					n--;
+					continue;
+				}
+				for (int k = 1; k < 5; k++)
+				{
+					oldcell = CellMap[ni][nj].Move(k);
+					if (oldcell != null) break;
+				}
+
+				//Console.WriteLine("-----"+n.ToString());
+				//for (int i = 0; i < 5; i++)
+				//{
+				//	for (int j = 0; j < 5; j++)
+				//	{
+				//		Console.Write(CellMap[i][j].Value.ToString());
+				//		Console.Write("\t");
+				//	}
+				//	Console.WriteLine();
+				//}
+			}
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					Console.Write(CellMap[i][j].Value.ToString());
+					Console.Write("\t");
+				}
+				Console.WriteLine();
+			}
+			string name = Console.ReadLine();
+			Console.WriteLine($"Fuck you, {name}");
+		}
+	}
+}
